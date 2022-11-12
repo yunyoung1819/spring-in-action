6f1d10777aacc7fa18001254d17126058f347ac2
yunyoung1819 <yunyoung1819@hanmail.net>
2022-11-12 23:54:23 +0900

    [feature] 서버에 데이터 변경 및 삭제 API 추가

--- tacoApp/tacocloud-api/src/main/java/tacos/web/api/DesignTacoController.java
+++ tacoApp/tacocloud-api/src/main/java/tacos/web/api/DesignTacoController.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.hateoas.EntityLinks;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import tacos.Taco;
 import tacos.data.TacoRepository;
 
@@ -29,5 +27,11 @@ public class DesignTacoController {
   public Iterable<Taco> recentTacos() {
     PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
     return tacoRepo.findAll(page).getContent();
+  }
+
+  @PostMapping(consumes = "application/json")
+  @ResponseStatus(HttpStatus.CREATED)
+  public Taco postTaco(@RequestBody Taco taco) {
+    return tacoRepo.save(taco);
   }
 }
--- tacoApp/tacocloud-web/src/main/java/tacos/web/OrderController.java
+++ tacoApp/tacocloud-web/src/main/java/tacos/web/OrderController.java
@@ -1,17 +1,15 @@
 package tacos.web;
 import javax.validation.Valid;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
 import tacos.Order;
@@ -23,13 +21,13 @@ import tacos.data.OrderRepository;
 @SessionAttributes("order")
 public class OrderController {
   
-  private OrderRepository orderRepo;
+  private OrderRepository repo;
 
   private OrderProps props;
 
-  public OrderController(OrderRepository orderRepo, 
+  public OrderController(OrderRepository repo,
           OrderProps props) {
-    this.orderRepo = orderRepo;
+    this.repo = repo;
     this.props = props;
   }
   
@@ -66,7 +64,7 @@ public class OrderController {
     
     order.setUser(user);
     
-    orderRepo.save(order);
+    repo.save(order);
     sessionStatus.setComplete();
     
     return "redirect:/";
@@ -78,9 +76,56 @@ public class OrderController {
 
     Pageable pageable = PageRequest.of(0, props.getPageSize());
     model.addAttribute("orders", 
-        orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+        repo.findByUserOrderByPlacedAtDesc(user, pageable));
     
     return "orderList";
+  }
+
+  @PutMapping("{orderId}")
+  public Order putOrder(@RequestBody Order order) {
+    return repo.save(order);
+  }
+
+  @PatchMapping(path = "/{orderId}", consumes = "application/json")
+  public Order patchOrder(@PathVariable("orderId") Long orderId,
+                          @RequestBody Order patch) {
+
+    Order order = repo.findById(orderId).get();
+
+    if (patch.getDeliveryName() != null) {
+      order.setDeliveryName(patch.getDeliveryName());
+    }
+    if (patch.getDeliveryStreet() != null) {
+      order.setDeliveryState(patch.getDeliveryStreet());
+    }
+    if (patch.getDeliveryCity() != null) {
+      order.setDeliveryCity(patch.getDeliveryCity());
+    }
+    if (patch.getDeliveryState() != null) {
+      order.setDeliveryState(patch.getDeliveryState());
+    }
+    if (patch.getDeliveryZip() != null) {
+      order.setDeliveryZip(patch.getDeliveryZip());
+    }
+    if (patch.getCcNumber() != null) {
+      order.setCcNumber(patch.getCcNumber());
+    }
+    if (patch.getCcExpiration() != null) {
+      order.setCcExpiration(patch.getCcExpiration());
+    }
+    if (patch.getCcCVV() != null) {
+      order.setCcCVV(patch.getCcCVV());
+    }
+
+    return repo.save(order);
+  }
+
+  @DeleteMapping("{orderId}")
+  @ResponseStatus(code = HttpStatus.NO_CONTENT)
+  public void deleteOrder(@PathVariable("orderId") Long orderId) {
+    try {
+      repo.deleteById(orderId);
+    } catch (EmptyResultDataAccessException e) {}
   }
 
 }
