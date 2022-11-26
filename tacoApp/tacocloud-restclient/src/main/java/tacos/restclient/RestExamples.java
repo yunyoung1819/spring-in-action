66f91f7fc1969d6208bc892f9ffbc33a7d843c46
yunyoung1819 <yunyoung1819@hanmail.net>
2022-11-27 03:09:11 +0900

    :sparkles: [feature] Traverson으로 REST API 사용하기

--- tacoApp/tacocloud-restclient/src/main/java/tacos/restclient/RestExamples.java
+++ tacoApp/tacocloud-restclient/src/main/java/tacos/restclient/RestExamples.java
@@ -5,7 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.client.Traverson;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 
 @SpringBootConfiguration
 @ComponentScan
@@ -19,5 +23,12 @@ public class RestExamples {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@Bean
+	public Traverson traverson() {
+		Traverson traverson = new Traverson(
+				URI.create("http://localhost:8080/api"), MediaTypes.HAL_JSON);
+		return traverson;
 	}
 }
--- tacoApp/tacocloud-restclient/src/main/java/tacos/restclient/TacoCloudClient.java
+++ tacoApp/tacocloud-restclient/src/main/java/tacos/restclient/TacoCloudClient.java
@@ -1,10 +1,16 @@
 package tacos.restclient;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.Resources;
 import org.springframework.hateoas.client.Traverson;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import tacos.Ingredient;
+
+import java.net.URI;
+import java.util.Collection;
 
 @Service
 @Slf4j
@@ -18,7 +24,6 @@ public class TacoCloudClient {
 		this.traverson = traverson;
 	}
 
-	// GET
 	public Ingredient getIngredientById(String ingredientId) {
 		return rest.getForObject("http://localhost:8080/ingredients/{id}", Ingredient.class, ingredientId);
 	}
@@ -38,13 +43,40 @@ public class TacoCloudClient {
 //		return rest.getForObject(url, Ingredient.class);
 //	}
 
-	// PUT
 	public void updateIngredient(Ingredient ingredient) {
 		rest.put("http://localhost:8080/ingredients/{id}", ingredient, ingredient.getId());
 	}
 
-	// DELETE
 	public void deleteIngredient(Ingredient ingredient) {
 		rest.delete("http://localhost:8080/ingredients/{id}", ingredient.getId());
 	}
+
+	public Ingredient createIngredient(Ingredient ingredient) {
+		return rest.postForObject("http://localhost:8080/ingredients", ingredient, Ingredient.class);
+	}
+
+//	public URI createIngredient(Ingredient ingredient) {
+//		return rest.postForLocation("http://localhost:8080/ingredients", ingredient);
+//	}
+
+//	public Ingredient createIngredient(Ingredient ingredient) {
+//		ResponseEntity<Ingredient> responseEntity =
+//				rest.postForEntity("http://localhost:8080/ingredients",
+//						ingredient,
+//						Ingredient.class);
+//
+//		log.info("New resource created at " + responseEntity.getHeaders().getLocation());
+//
+//		return responseEntity.getBody();
+//	}
+
+	ParameterizedTypeReference<Resources<Ingredient>> ingredientType =
+			new ParameterizedTypeReference<Resources<Ingredient>>() {};
+
+	Resources<Ingredient> ingredientRes =
+			traverson
+					.follow("ingredients")
+					.toObject(ingredientType);
+
+	Collection<Ingredient> ingredients = ingredientRes.getContent();
 }
