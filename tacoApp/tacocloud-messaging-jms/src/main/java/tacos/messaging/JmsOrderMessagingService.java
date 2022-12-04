e06e80c7d9a924e1ea28ab83ba1285e5ff472d8e
yunyoung1819 <yunyoung1819@hanmail.net>
2022-12-04 14:27:08 +0900

    :sparkles: [feature] taco클라우드 JmsTemplate을 사용한 메시지 전송 기능 구현

---
+++ tacoApp/tacocloud-messaging-jms/src/main/java/tacos/messaging/JmsOrderMessagingService.java
@@ -0,0 +1,34 @@
+package tacos.messaging;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.stereotype.Service;
+import tacos.Order;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
+/**
+ * send()를 사용해서 주문 데이터 전송하기
+ */
+@Service
+public class JmsOrderMessagingService implements OrderMessagingService {
+	private JmsTemplate jms;
+
+	@Autowired
+	public JmsOrderMessagingService(JmsTemplate jms) {
+		this.jms = jms;
+	}
+
+	@Override
+	public void sendOrder(Order order) {
+		jms.send(new MessageCreator() {
+			@Override
+			public Message createMessage(Session session) throws JMSException {
+				return session.createObjectMessage(order);
+			}
+		});
+	}
+}
