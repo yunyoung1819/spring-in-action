514d8a6ccc5ed4cb73a00dd10dc81b56745f8d46
yunyoung1819 <yunyoung1819@hanmail.net>
2023-03-20 20:29:54 +0900

    :sparkles: [feature] 스프링 WebFlux 사용: 리액티브 컨트롤러 작성

--- tacoApp/tacocloud-api/src/main/java/tacos/web/api/DesignTacoController.java
+++ tacoApp/tacocloud-api/src/main/java/tacos/web/api/DesignTacoController.java
@@ -1,20 +1,11 @@
 package tacos.web.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.hateoas.EntityLinks;
-import org.springframework.hateoas.Resource;
-import org.springframework.hateoas.Resources;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import tacos.Taco;
 import tacos.data.TacoRepository;
-
-import java.util.List;
-
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 
 @RestController
@@ -30,24 +21,29 @@ public class DesignTacoController {
     this.tacoRepo = tacoRepo;
   }
 
+//  @GetMapping("/recent")
+//  public Resources<TacoResource> recentTacos() {
+//    PageRequest page = PageRequest.of(
+//            0, 12, Sort.by("createdAt").descending());
+//    List<Taco> tacos = tacoRepo.findAll(page).getContent();
+//
+//    List<TacoResource> tacoResources = new TacoResourceAssembler().toResources(tacos);
+//    Resources<TacoResource> recentResources = new Resources<TacoResource>(tacoResources);
+//
+//    recentResources.add(
+//            linkTo(methodOn(DesignTacoController.class).recentTacos())
+//                    .withRel("recents"));
+//    return recentResources;
+//  }
+
   @GetMapping("/recent")
-  public Resources<TacoResource> recentTacos() {
-    PageRequest page = PageRequest.of(
-            0, 12, Sort.by("createdAt").descending());
-    List<Taco> tacos = tacoRepo.findAll(page).getContent();
-
-    List<TacoResource> tacoResources = new TacoResourceAssembler().toResources(tacos);
-    Resources<TacoResource> recentResources = new Resources<TacoResource>(tacoResources);
-
-    recentResources.add(
-            linkTo(methodOn(DesignTacoController.class).recentTacos())
-                    .withRel("recents"));
-    return recentResources;
+  public Flux<Taco> recentTacos() {
+    return tacoRepo.findAll().take(12);
   }
 
-  @PostMapping(consumes = "application/json")
-  @ResponseStatus(HttpStatus.CREATED)
-  public Taco postTaco(@RequestBody Taco taco) {
-    return tacoRepo.save(taco);
-  }
+//  @PostMapping(consumes = "application/json")
+//  @ResponseStatus(HttpStatus.CREATED)
+//  public Taco postTaco(@RequestBody Taco taco) {
+//    return tacoRepo.save(taco);
+//  }
 }
--- tacoApp/tacocloud-data/src/main/java/tacos/data/TacoRepository.java
+++ tacoApp/tacocloud-data/src/main/java/tacos/data/TacoRepository.java
@@ -1,11 +1,10 @@
 package tacos.data;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
-
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import tacos.Taco;
 
 
 public interface TacoRepository 
-         extends PagingAndSortingRepository<Taco, Long> {
+         extends ReactiveCrudRepository<Taco, Long> {
 
 }
